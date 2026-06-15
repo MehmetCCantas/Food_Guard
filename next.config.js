4a2b5b2693531ce917956bfac275bf483cd0a144
Mehmet Can Cantas <cantasmehmetcan@gmail.com>
2026-06-16 01:35:40 +0300

    fix: remove invalid rewrites from next.config.js

--- next.config.js
+++ next.config.js
@@ -15,14 +15,7 @@ const nextConfig = {
                 hostname: '127.0.0.1',
             },
         ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'}/:path*`,
-            },
-        ];
+        unoptimized: true,
     },
 };
 

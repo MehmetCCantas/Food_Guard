d02827891455fd602b25438f46be687b6562d8df
Mehmet Can Cantas <cantasmehmetcan@gmail.com>
2026-06-16 01:39:35 +0300

    fix: simplify next.config.js image config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-            },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-            },
-        ],
         unoptimized: true,
     },
 };
